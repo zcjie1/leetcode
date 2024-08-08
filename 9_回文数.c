3aebdc46359d7070aa737ca62a887f95bea06ec9
zcj <2399510409@qq.com>
2024-08-08 19:39:45 +0800

    回文数 字符串解法

---
+++ 9_回文数.c
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=c
+ *
+ * [9] 回文数
+ *
+ * https://leetcode.cn/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (56.16%)
+ * Likes:    2878
+ * Dislikes: 0
+ * Total Accepted:    1.7M
+ * Total Submissions: 3M
+ * Testcase Example:  '121'
+ *
+ * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+ * 
+ * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * 
+ * 
+ * 例如，121 是回文，而 123 不是。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：x = 121
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：x = -121
+ * 输出：false
+ * 解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：x = 10
+ * 输出：false
+ * 解释：从右向左读, 为 01 。因此它不是一个回文数。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * -2^31 <= x <= 2^31 - 1
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你能不将整数转为字符串来解决这个问题吗？
+ * 
+ */
+
+// @lc code=start
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPalindrome(int x) {
+    int num[32];
+    int count = 0;
+    int curr = 0;
+    int left, right;
+
+    if(x < 0)
+        return false;
+    
+    if(x == 0)
+        return true;
+    
+    if(!(x % 10))
+        return false;
+
+    while(x) {
+        curr = x % 10;
+        num[count++] = curr;
+        x = x / 10;
+    }
+
+    left = 0;
+    right = count-1;
+
+    while(left < right) {
+        if(num[left] == num[right]) {
+            left++;
+            right--;
+            continue;
+        }
+        return false;
+    }
+    return true;
+}
+// @lc code=end
+
