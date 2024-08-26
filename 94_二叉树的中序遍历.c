16d405459b91ffb33d1b0922ff16d7a132a02888
zcj <2399510409@qq.com>
2024-08-26 09:31:00 +0800

    二叉树中序遍历递归解法

---
+++ 94_二叉树的中序遍历.c
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=c
+ *
+ * [94] 二叉树的中序遍历
+ *
+ * https://leetcode.cn/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Easy (77.12%)
+ * Likes:    2129
+ * Dislikes: 0
+ * Total Accepted:    1.5M
+ * Total Submissions: 2M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ * 
+ * 
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+// @lc code=start
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+void inorder(struct TreeNode* root, int* res, int* resSize) {
+    if (root==NULL)
+        return;
+    inorder(root->left, res, resSize);
+    res[(*resSize)++] = root->val;
+    inorder(root->right, res, resSize);
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* res = malloc(sizeof(int) * 101);
+    *returnSize = 0;
+    inorder(root, res, returnSize);
+    return res;
+}
+// @lc code=end
+
